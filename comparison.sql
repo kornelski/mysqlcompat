dc79cce04b7bb943731df2954c7e2c03410b50b9
chriskl <chriskl>
2005-11-25 08:07:52 +0000

    nicer version of null_safe_cmp

--- comparison.sql
+++ comparison.sql
@@ -3,13 +3,9 @@
 CREATE OR REPLACE FUNCTION null_safe_cmp(anyelement, anyelement)
 RETURNS integer AS '
   SELECT CASE
-    WHEN $1 IS NULL AND $2 IS NULL THEN 1
-    WHEN $1 IS NOT NULL != $2 IS NOT NULL THEN 0
-    ELSE CASE 
-      WHEN $1 = $2 THEN 1 
-      ELSE 0
-    END
-  END
+    WHEN ($1 IS DISTINCT FROM $2) THEN 0
+    ELSE 1
+  END 
 ' IMMUTABLE LANGUAGE SQL;
 
 CREATE OPERATOR <=> (
